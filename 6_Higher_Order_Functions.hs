fee83c1bc463734ffa33786c58624e6b54a8b481
bernborgess <62393097+bernborgess@users.noreply.github.com>
2022-04-06 14:27:51 -0300

    6_Higher Order Funtions

---
+++ 6_Higher_Order_Functions.hs
@@ -0,0 +1,46 @@
+Higher Order Functions
+
+-- apply function that takes a and returns b
+app :: (a -> b) -> a -> b
+app f x = f x
+
+add1 :: Int -> Int
+add1 x = x + 1
+
+-- app add1 1
+
+-- Anonymous Functions
+-- (\<args> -> <expr>)
+
+-- add1 = (\x -> x+1)
+
+-- (\x y z -> x+y+z) 1 2 3
+-- => 6
+
+app (\x -> x+1) 1
+ -- => 2
+
+
+-- Map = converts a list to another
+map :: (a -> b) -> [a] -> [b]
+
+-- map (\x -> x+1) [1,2,3,4]
+
+map (\(x,y) -> x+y) [(1,2),(2,3),(3,4)]
+-- => [3,5,7]
+
+-- Filter = takes a bool function (predicate)
+-- and a list, return the list with the matches only
+filter :: (a -> Bool) -> [a] -> [a]
+
+filter (\(x,y) -> x /= y) [(1,2),(2,2)]
+ -- => (1,2)
+
+
+
+
+
+
+
+
+
