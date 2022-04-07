2e8d334cab68713001c2dca1b04c77e1a27687eb
bernborgess <bernborgess@outlook.com>
2022-04-06 23:29:38 -0300

    9_Folding

---
+++ 9_Folding.hs
@@ -0,0 +1,38 @@
+-- ? Folding
+
+-- foldr :: (a -> b -> b) -> b -> [a] -> b
+
+-- foldr (⊕) a [x1,x2,...,xn] = x1 ⊕ x2 ⊕ ... ⊕ xn ⊕ a
+-- ? a = accumulator
+-- ? ⊕ = binary function
+
+-- Example: partial functions
+sum = foldr (+) 0
+and = foldr (&&) True
+or = foldr (||) False
+
+-- foldr (\elem acc -> <term>) <start_acc> <list>
+
+count e =
+  foldr (\x acc -> if e==x then acc+1 else acc) 0
+
+isAll e = foldr (\x -> (&&) $ e==x) True
+isAll e = foldr (\x acc -> e==x && acc) True
+
+length = foldr (\x -> (+) 1) 0
+map f = foldr ((:) . f) []
+
+
+-- Folding (Direction)
+foldr (\elem acc -> <term>) <start_acc> <list>
+
+--  1 + (2 + (3 + (4 + (5 + 0))))
+
+foldl (\acc elem -> <term>) <start_acc> <list>
+--  ((((0 + 1) + 2) + 3) + 4) + 5
+
+-- Folding (Tree)
+
+
+
+-- ⊕⊕⊕
