d41fc3ce270e063a284f5e5fd5347bf619f1b020
bernborgess <62393097+bernborgess@users.noreply.github.com>
2022-04-11 14:17:19 -0300

    11 Folding exercises pt2

--- 11_Folding_Exercises.hs
+++ 11_Folding_Exercises.hs
@@ -25,3 +25,50 @@ prefixes =
 
 
 
+-- Exercise #3
+-- Given a set of k+1 data points
+-- where no two xj are the same, the interpolation
+-- polynomial in the Lagrange form is a linear 
+-- combination 
+-- L(x) = SUM(j=0;k){yjlj(x)}
+-- of Lagrange basis polynomials
+--
+-- lj(x) = PROD(0<=m<=k;m!=j){(x-xm)/(xj-xm)}
+
+lagrange :: [(Float,Float)] -> Float -> Float
+
+
+lagrange xs x = foldl (\acc (xj,y) -> acc + (y*l xj)) 0
+xs 
+ where
+  l xj = foldl (
+    \acc (xk,_) ->
+      if xj==xk then 
+        acc
+      else
+        acc * ((x-xk)/(xj-xk))
+  ) 1 xs
+
+
+
+
+-- Exercise #4 
+-- Create a function foldtrie that folds
+-- the elements of a trie in a preorder traversel
+
+data Trie a = Leaf a | Node a [Trie a]
+
+-- ex:
+-- t = 
+-- Node 'c' [
+-- Node 'a'
+--   [Leaf 'r', Leaf 't'],
+-- Node 'o'
+--   [Node 'o'
+--     [Leaf 'l']]]
+
+foldtrie :: (b -> a -> b) -> b -> Trie a -> b
+foldtrie f acc (Leaf x) = f acc x
+foldtrie f acc (Node x xs) = foldl f' (f acc x) xs
+  where
+    f' acc t = foldtrie f acc t
