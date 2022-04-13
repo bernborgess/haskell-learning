204689693cf47899e255a15e957022394d417157
bernborgess <62393097+bernborgess@users.noreply.github.com>
2022-04-13 13:49:20 -0300

    Higher order review

--- 6_Higher_Order_Functions.hs
+++ 6_Higher_Order_Functions.hs
@@ -8,23 +8,28 @@ add1 :: Int -> Int
 add1 x = x + 1
 
 -- app add1 1
+-- => 2
 
 -- Anonymous Functions
 -- (\<args> -> <expr>)
 
 -- add1 = (\x -> x+1)
+-- => 2
 
 -- (\x y z -> x+y+z) 1 2 3
 -- => 6
 
+-- Higher Order + Anonymous
 app (\x -> x+1) 1
  -- => 2
 
 
--- Map = converts a list to another
+-- Map = converts a list to another, applying a 
+-- to each element
 map :: (a -> b) -> [a] -> [b]
 
--- map (\x -> x+1) [1,2,3,4]
+-- map (\x -> x+1) [1,2,3,4,5]
+-- => [2,3,4,5,6]
 
 map (\(x,y) -> x+y) [(1,2),(2,3),(3,4)]
 -- => [3,5,7]
@@ -34,7 +39,10 @@ map (\(x,y) -> x+y) [(1,2),(2,3),(3,4)]
 filter :: (a -> Bool) -> [a] -> [a]
 
 filter (\(x,y) -> x /= y) [(1,2),(2,2)]
- -- => (1,2)
+ -- => (1,2)   
+
+filter (\x -> x > 2) [1,2,3,4,5]
+-- => [3,4,5k]
 
 
 
