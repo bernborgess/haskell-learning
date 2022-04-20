8e08f4323bbca6265dd5b26f6b1f7a82d5debf67
bernborgess <bernborgess@outlook.com>
2022-04-20 11:30:10 -0300

    pratica IO

---
+++ 231a.hs
@@ -0,0 +1,72 @@
+--
+--    author:  bernborgess
+--    problem: a - cf
+--    created: 20.April.2022 09:56:17
+--
+
+-- https://codeforces.com/contest/231/problem/A
+
+import Control.Monad
+
+
+solve :: [Int] -> Int
+solve [] = 0
+solve (x:xs) = x + solve xs
+
+-- INPUT:
+-- 3          <- numero de tuplas     (n)
+-- 1 0 1      <- tupla (Int,Int,Int)
+-- 0 0 1
+-- 1 1 1
+
+
+-- O que eu queria fazer:
+
+main :: IO ()
+main = do
+  sn <- getLine
+  let n = [1..(read sn::Int)]
+
+
+
+  ls <- getLine
+  
+-- let in1 = read ls :: [Int]
+
+  return ()
+
+
+
+
+
+  -- let out = map (\x -> show (2*x) ) tc
+
+
+
+
+
+
+
+
+
+
+  -- -- ? fn Int =>  :: [Int]
+  -- let fn tc = solve . map read . words <$> getLine
+
+  -- ans <- foldr (\x acc -> (fn x) +acc) 0 tc
+  -- -- ans <- fn (head tc)
+  -- -- let ans = mapM_ solve tc
+  -- -- let out = sum fn tc
+  -- putStrLn (show ans)
+  -- return ()
+
+
+  --   print sum . mapM_ fn ids
+  -- where ids   = enumFromTo 1 . read <$> getLine
+  --       fn tc = solve . map read . words <$> getLine
+
+
+-- main :: IO ()
+-- main = do ids >>= mapM_ (fn >=> print)
+--   where ids   = enumFromTo 1 . read <$> getLine
+--         fn tc = solve . map read . words <$> getLine
--- 6_Higher_Order_Functions.hs
+++ 6_Higher_Order_Functions.hs
@@ -1,4 +1,4 @@
-Higher Order Functions
+-- Higher Order Functions
 
 -- apply function that takes a and returns b
 app :: (a -> b) -> a -> b
@@ -20,28 +20,28 @@ add1 x = x + 1
 -- => 6
 
 -- Higher Order + Anonymous
-app (\x -> x+1) 1
+-- app (\x -> x+1) 1
  -- => 2
 
 
 -- Map = converts a list to another, applying a 
 -- to each element
-map :: (a -> b) -> [a] -> [b]
+-- map :: (a -> b) -> [a] -> [b]
 
 -- map (\x -> x+1) [1,2,3,4,5]
 -- => [2,3,4,5,6]
 
-map (\(x,y) -> x+y) [(1,2),(2,3),(3,4)]
+-- map (\(x,y) -> x+y) [(1,2),(2,3),(3,4)]
 -- => [3,5,7]
 
 -- Filter = takes a bool function (predicate)
 -- and a list, return the list with the matches only
-filter :: (a -> Bool) -> [a] -> [a]
+-- filter :: (a -> Bool) -> [a] -> [a]
 
-filter (\(x,y) -> x /= y) [(1,2),(2,2)]
+-- filter (\(x,y) -> x /= y) [(1,2),(2,2)]
  -- => (1,2)   
 
-filter (\x -> x > 2) [1,2,3,4,5]
+-- filter (\x -> x > 2) [1,2,3,4,5]
 -- => [3,4,5]
 
 
