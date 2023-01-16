479cdff2481f7dbee3284fd417d9a5ce2ff1f404
bernborgess <bernborgess@outlook.com>
2023-01-16 18:58:14 -0300

    cleaner

--- Tsoding/tictactoe/demo.hs
+++
@@ -1,8 +0,0 @@
-import Graphics.Gloss
-
-main :: IO ()
-main = display window background drawing
-  where
-    window = InWindow "Nice Window" (200,200) (0,0)
-    background = white
-    drawing = Circle 80
