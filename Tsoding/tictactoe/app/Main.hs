7041e59ef194328e1cdaaab47c963ff9945ab5a9
bernborgess <bernborgess@outlook.com>
2023-01-16 18:33:40 -0300

    clean again

--- Tsoding/tictactoe/app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main (main) where
-
-import Lib
-
-main :: IO ()
-main = do
-  n <- getLine
-  putStrLn (n++" is "++n)
---
+++ Tsoding/tictactoe/demo.hs
@@ -0,0 +1,8 @@
+import Graphics.Gloss
+
+main :: IO ()
+main = display window background drawing
+  where
+    window = InWindow "Nice Window" (200,200) (0,0)
+    background = white
+    drawing = Circle 80
