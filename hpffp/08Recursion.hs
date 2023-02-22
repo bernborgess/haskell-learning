6c79cbf9fb0a777713e73b8cf87a3553b04da38d
bernborgess <bernborgess@outlook.com>
2023-02-22 13:08:00 -0300

    recursion

---
+++ hpffp/08Recursion.hs
@@ -0,0 +1,34 @@
+module WordNumber where
+
+import Data.List (intercalate)
+
+digitToWord :: Int -> String
+digitToWord n = case n of
+  0 -> "zero"
+  1 -> "one"
+  2 -> "two"
+  3 -> "three"
+  4 -> "four"
+  5 -> "five"
+  6 -> "six"
+  7 -> "seven"
+  8 -> "eight"
+  9 -> "nine"
+  _ -> "error"
+
+digits :: Int -> [Int]
+digits = reverse . go
+  where
+    go n
+      | n < 10 = [n]
+      | otherwise =
+          let (d, m) = divMod n 10
+              rs = go d
+           in m : rs
+
+wordNumber :: Int -> String
+-- wordNumber n = concat $ intersperse "-" $ map digitToWord $ digits n
+wordNumber = intercalate "-" . map digitToWord . digits
+
+-- ? wordNumber 12324546
+-- => "one-two-three-two-four-five-four-six"
