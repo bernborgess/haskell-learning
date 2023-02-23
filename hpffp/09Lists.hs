d45fa84dc2076eb018dbd4a0eb5ebe903107187a
bernborgess <bernborgess@outlook.com>
2023-02-23 09:55:39 -0300

    Char list exercises

--- hpffp/09Lists.hs
+++ hpffp/09Lists.hs
@@ -1,3 +1,5 @@
+import Data.Char
+
 -- Write your own `enumFromTo` definitions for the types
 -- provided. Do not use ranges syntax to do so. It should
 -- return the same results as if you did [start..stop]
@@ -208,3 +210,59 @@ myZipWith f (a : as) (b : bs) = f a b : myZipWith f as bs
 -- 3. Rewrite your zip in terms of the zipWith you wrote.
 myZip' :: [a] -> [b] -> [(a, b)]
 myZip' = myZipWith (,)
+
+-- =============================================
+-- Chapter Exercises
+-- ? Data.Char
+-- These first few exercises are straightforward but will introduce you to
+-- some new library functions and review some of what we’ve learned so
+-- far. Some of the functions we will use here are not standard in Prelude
+-- and so have to be imported from a module called Data.Char. You may
+-- do so in a source file (recommended) or at the Prelude prompt with
+-- the same phrase: import Data.Char (write that at the top of your
+-- source file). This brings into scope a bunch of new standard functions
+-- we can play with that operate on Char and String types
+
+-- 1. Query the types of isUpper and toUpper.
+-- isUpper :: Char -> Bool
+-- toUpper :: Char -> Char
+
+-- 2. Given the following behaviors, which would we use to write
+-- a function that filters all the uppercase letters out of a String?
+-- Write that function such that, given the input “HbEfLrLxO,” your
+-- function will return “HELLO.”
+-- Prelude Data.Char> isUpper 'J'
+-- True
+-- Prelude Data.Char> toUpper 'j'
+-- 'J'
+filter2 :: String -> String
+filter2 = filter isUpper
+
+-- 3. Write a function that will capitalize the first letter of a String
+-- and return the entire String. For example, if given the argument
+-- “julie,” it will return “Julie.”
+capFst :: String -> String
+capFst "" = ""
+capFst (x : xs) = toUpper x : xs
+
+-- 4. Now make a new version of that function that is recursive such
+-- that if you give it the input “woot” it will holler back at you
+-- “WOOT.” The type signature won’t change, but you will want to
+-- add a base case.
+capAll :: String -> String
+capAll = map toUpper
+
+-- 5. To do the final exercise in this section, we’ll need another standard
+-- function for lists called head. Query the type of head and
+-- experiment with it to see what it does.
+-- ? head :: [a] -> a
+-- Now write a function that
+-- will capitalize the first letter of a String and return only that letter
+-- as the result.
+capHead :: String -> Char
+capHead (x : xs) = toUpper x
+
+-- 6. Cool. Good work. Now rewrite it as a composed function. Then,
+-- for fun, rewrite it pointfree.
+capHead' :: String -> Char
+capHead' = toUpper . head
