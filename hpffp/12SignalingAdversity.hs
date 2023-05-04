2788be2a24527eaf42e52effe9adf14ad6389d56
bernborgess <bernborgess@outlook.com>
2023-05-03 22:43:50 -0300

    nat

--- hpffp/12SignalingAdversity.hs
+++ hpffp/12SignalingAdversity.hs
@@ -145,3 +145,32 @@ countVowels :: String -> Integer
 countVowels = fromIntegral . length . filter isVowel
   where
     isVowel c = toLower c `elem` "aeiou"
+
+{-
+It’s only Natural
+You’ll be presented with a datatype to represent the natural numbers.
+The only values representable with the naturals are whole numbers
+from zero to infinity. Your task will be to implement functions to
+convert Naturals to Integers and Integers to Naturals. The conversion
+from Naturals to Integers won’t return Maybe because Integers are
+a strict superset of Naturals. Any Natural can be represented by an
+Integer, but the same is not true of any Integer. Negative numbers are
+not valid natural numbers
+-}
+
+data Nat
+  = Zero
+  | Succ Nat
+  deriving (Eq, Show)
+
+natToInteger :: Nat -> Integer
+natToInteger Zero = 0
+natToInteger (Succ n) = 1 + natToInteger n
+
+integerToNat :: Integer -> Maybe Nat
+integerToNat i
+  | i < 0 = Nothing
+  | otherwise = Just $ i2n i
+  where
+    i2n 0 = Zero
+    i2n n = Succ (i2n (n - 1))
