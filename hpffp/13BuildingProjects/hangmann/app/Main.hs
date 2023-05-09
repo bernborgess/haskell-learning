61555fa5f32e1f79c7f747a5340094ec12b54b21
bernborgess <bernborgess@outlook.com>
2023-05-09 16:41:01 -0300

    new deps

--- hpffp/13BuildingProjects/hangmann/app/Main.hs
+++ hpffp/13BuildingProjects/hangmann/app/Main.hs
@@ -1,4 +1,12 @@
 module Main where
 
+import Control.Monad (forever)
+import Data.Char (toLower)
+import Data.Maybe (isJust)
+import Data.List (intersperse)
+import System.Exit (exitSuccess)
+import System.Random (randomRIO)
+
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
