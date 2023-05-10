e901766308ff4db89de8ff40e4d4429ffcf22622
bernborgess <bernborgess@outlook.com>
2023-05-10 13:26:10 -0300

    just base

--- hpffp/13BuildingProjects/hangmann/app/Main.hs
+++ hpffp/13BuildingProjects/hangmann/app/Main.hs
@@ -4,8 +4,8 @@ import Control.Monad (forever)
 import Data.Char (toLower)
 import Data.Maybe (isJust)
 import Data.List (intersperse)
-import System.Exit (exitSuccess)
-import System.Random (randomRIO)
+-- import System.Exit (exitSuccess)
+-- import System.Random (randomRIO)
 
 
 main :: IO ()
