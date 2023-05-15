a892e2193c0253f9527ed2c94e827271f3a3ffdb
bernborgess <bernborgess@outlook.com>
2023-05-15 20:30:55 -0300

    short exercise

--- hpffp/14Testing/addition/src/Addition.hs
+++ hpffp/14Testing/addition/src/Addition.hs
@@ -24,3 +24,25 @@ dividedBy num denom = go num denom 0
     go n d count
       | n < d = (count, n)
       | otherwise = go (n - d) d (count + 1)
+
+multi :: (Ord a, Num a) => a -> a -> a
+multi 0 _ = 0
+multi _ 0 = 0
+multi 1 y = y
+multi x 1 = x
+multi x y
+  | x < 0 = -(multi (-x) y)
+  | y < 0 = -(multi x (-y))
+  | otherwise = y + multi (x - 1) y
+
+testMulti :: IO ()
+testMulti = hspec $ do
+  describe "Small numbers" $ do
+    it "1 * 1 == 1" $ do
+      multi 1 1 `shouldBe` 1
+  describe "Negative numbers" $ do
+    it "(-1) * (-1) == 1" $ do
+      multi (-1) (-1) `shouldBe` 1
+  describe "Big numbers" $ do
+    it "10000 * 10000 == 100000000" $ do
+      multi 10000 10000 `shouldBe` 100000000
