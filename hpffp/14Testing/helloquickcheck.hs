440430350310b5dbd8b32063a84705d173f21f75
bernborgess <bernborgess@outlook.com>
2023-05-15 11:02:35 -0300

    helloquickcheck

--- hpffp/14Testing/helloquickcheck.hs
+++ hpffp/14Testing/helloquickcheck.hs
@@ -1,4 +1,5 @@
 import Test.QuickCheck
+import Prelude hiding (lookup)
 
 propSum a b = (a + b) === (b + a)
 
@@ -12,4 +13,24 @@ rev = rev_aux []
     rev_aux acc [] = acc
     rev_aux acc (x : xs) = rev_aux (x : acc) xs
 
-propRev xs = reverse xs === rev xs
+-- propRev xs = collect (length xs) $ reverse xs === revWrong xs
+
+propRev :: (Eq a, Show a) => [a] -> Property
+propRev xs = classify (null xs) "empty" $ reverse xs === rev xs
+
+test4 :: IO ()
+test4 = quickCheck $ withMaxSuccess 10000 prop
+  where
+    prop = propRev :: [Int] -> Property
+
+lookup :: Eq a => a -> [(a, b)] -> Maybe b
+lookup k [] = Nothing
+lookup k ((k', v) : m)
+  | k == k' = Just v
+  | otherwise = lookup k m
+
+propLookup k v m = lookup k ((k, v) : m) === Just v
+  where
+    types = (k :: Int, v :: Int)
+
+-- quickCheck $ verbose propLookup
