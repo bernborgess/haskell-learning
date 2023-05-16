b0186b3c91bc2335274853af93ea4b30f567d5ee
bernborgess <bernborgess@outlook.com>
2023-05-16 18:44:17 -0300

    testing length

--- hpffp/08Recursion/tests/tests.hs
+++ hpffp/08Recursion/tests/tests.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import Test.Hspec
+import Test.QuickCheck
 import WordNumber (digitToWord, digits, wordNumber)
 
 main :: IO ()
@@ -9,14 +10,24 @@ main = hspec $ do
     it "returns zero for 0" $ do
       digitToWord 0 `shouldBe` "zero"
     it "returns one for 1" $ do
-      print "???"
+      digitToWord 1 `shouldBe` "one"
+
   describe "digits does what we want" $ do
     it "returns [1] for 1" $ do
       digits 1 `shouldBe` [1]
     it "returns [1, 0, 0] for 100" $ do
-      print "???"
+      digits 100 `shouldBe` [1, 0, 0]
+    it "has the right length" $ do
+      let prop n = ds === lg
+            where
+              ds = length $ digits n
+              lg = floor $ logBase (10 :: Double) $ fromIntegral (n :: Int)
+      property prop
+
+  -- property $ (\n -> (length $ digits n) === logBase 10 n)
+
   describe "wordNumber does what we want" $ do
     it "returns one-zero-zero for 100" $ do
       wordNumber 100 `shouldBe` "one-zero-zero"
     it "returns nine-zero-zero-one for 9001" $ do
-      print "???"
+      wordNumber 9001 `shouldBe` "nine-zero-zero-one"
