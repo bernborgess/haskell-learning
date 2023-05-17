f9679c0a907d1c73435efdb5340b097735e42744
bernborgess <bernborgess@outlook.com>
2023-05-16 21:50:29 -0300

    last of test

--- hpffp/08Recursion/tests/tests.hs
+++ hpffp/08Recursion/tests/tests.hs
@@ -18,16 +18,18 @@ main = hspec $ do
     it "returns [1, 0, 0] for 100" $ do
       digits 100 `shouldBe` [1, 0, 0]
     it "has the right length" $ do
-      let prop n = ds === lg
-            where
-              ds = length $ digits n
-              lg = floor $ logBase (10 :: Double) $ fromIntegral (n :: Int)
+      let log10 x = if x < 10 then 0 else 1 + log10 (x `div` 10)
+      let prop n = length (digits n) === 1 + log10 n
       property prop
-
-  -- property $ (\n -> (length $ digits n) === logBase 10 n)
+    it "has the right value" $ do
+      let fn acc x = 10 * acc + x
+      let prop n = n === foldl fn 0 (digits n)
+      property prop
 
   describe "wordNumber does what we want" $ do
     it "returns one-zero-zero for 100" $ do
       wordNumber 100 `shouldBe` "one-zero-zero"
     it "returns nine-zero-zero-one for 9001" $ do
       wordNumber 9001 `shouldBe` "nine-zero-zero-one"
+    it "returns zero for 0" $ do
+      wordNumber 0 `shouldBe` "zero"
