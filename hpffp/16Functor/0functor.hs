e33b4ba9306b72c01b6c74bc50d843815d7e9dfb
bernborgess <bernborgess@outlook.com>
2023-06-07 23:09:33 -0300

    Wrap

--- hpffp/16Functor/0functor.hs
+++ hpffp/16Functor/0functor.hs
@@ -142,13 +142,60 @@ instance Arbitrary a => Arbitrary (Possibly a) where
         (2, Yeppers <$> arbitrary)
       ]
 
+-- Short Exercise (Again)
+-- 1. Write a Functor instance for a datatype identical to Either. We’ll
+-- use our own datatype because Either also already has a Functor
+-- instance.
+data Sum a b
+  = First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second $ f b
+
+instance
+  (Arbitrary a, Arbitrary b) =>
+  Arbitrary (Sum a b)
+  where
+  arbitrary =
+    frequency
+      [ (1, First <$> arbitrary),
+        (1, Second <$> arbitrary)
+      ]
+
+-- 2. Why is a Functor instance that applies the function only to First,
+-- Either’s Left, impossible? We covered this earlier.
+-- ? Again, because of the Kind of a Functor (*->*)
+
+newtype Constant a b = Constant {getConstant :: a}
+  deriving (Eq, Show)
+
+instance Functor (Constant m) where
+  fmap _ (Constant v) = Constant v
+
+instance
+  (Arbitrary a, Arbitrary b) =>
+  Arbitrary (Constant a b)
+  where
+  arbitrary = Constant <$> arbitrary
+
 -- My Functor
-type MF = Possibly Int
+-- type MF = Constant Int Int
 
-ch1 = quickCheck $ \x -> functorIdentity (x :: MF)
+-- ch1 = quickCheck $ \x -> functorIdentity (x :: MF)
 
-type IntToInt = Fun Int Int
+-- type IntToInt = Fun Int Int
 
-type IntFC = MF -> IntToInt -> IntToInt -> Bool
+-- type IntFC = MF -> IntToInt -> IntToInt -> Bool
 
-ch2 = quickCheck (functorCompose' :: IntFC)
+-- ch2 = quickCheck (functorCompose' :: IntFC)
+
+-- More Structure, more functors
+newtype Wrap f a
+  = Wrap (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (Wrap f) where
+  fmap f (Wrap fa) = Wrap (fmap f fa)
