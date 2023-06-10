b09309d1222be6027ba374e779adfdbbeb5cfcc3
bernborgess <bernborgess@outlook.com>
2023-06-10 01:30:12 -0300

    windows workaround

--- skillsMatter/movs/app/Main.hs
+++ skillsMatter/movs/app/Main.hs
@@ -1,12 +1,23 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
 module Main where
 
+import Data.Char
+import Foreign.C.Types
 import System.IO
+
+getHiddenChar = chr . fromEnum <$> c_getch
+
+foreign import ccall unsafe "conio.h getch"
+  c_getch :: IO CInt
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
   hSetEcho stdin False
-  mc <- getChar
+  putStr "$> "
+  mc <- getHiddenChar
   case mc of
     ' ' -> do
       print "space"
