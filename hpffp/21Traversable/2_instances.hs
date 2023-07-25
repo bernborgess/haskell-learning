278621c1f9c7287755a2afaa0ea6b2fb46d1f5d0
bernborgess <bernborgess@outlook.com>
2023-07-25 10:31:09 -0300

    traversable chapter done

--- hpffp/21Traversable/2_instances.hs
+++ hpffp/21Traversable/2_instances.hs
@@ -249,8 +249,43 @@ instance (Arbitrary a, Arbitrary (n a)) => Arbitrary (S n a) where
 instance (Eq (n a), Eq a) => EqProp (S n a) where
     (=-=) = eq
 
-type TI = S Maybe
+-- ? Tree
+data Tree a
+    = Empty
+    | Leaf a
+    | Node (Tree a) a (Tree a)
+    deriving (Eq, Show)
+
+instance Functor Tree where
+    fmap _ Empty = Empty
+    fmap f (Leaf a) = Leaf (f a)
+    fmap f (Node l a r) = Node (f <$> l) (f a) (f <$> r)
+
+instance Foldable Tree where
+    foldMap :: Monoid m => (a -> m) -> Tree a -> m
+    foldMap _ Empty = mempty
+    foldMap f (Leaf a) = f a
+    foldMap f (Node l a r) = foldMap f l <> f a <> foldMap f r
+
+instance Traversable Tree where
+    traverse :: Applicative f => (a -> f b) -> Tree a -> f (Tree b)
+    traverse _ Empty = pure Empty
+    traverse f (Leaf a) = Leaf <$> f a
+    traverse f (Node l a r) = Node <$> traverse f l <*> f a <*> traverse f r
+
+instance Arbitrary a => Arbitrary (Tree a) where
+    arbitrary =
+        frequency
+            [ (1, pure Empty)
+            , (2, Leaf <$> arbitrary)
+            , (3, Node <$> arbitrary <*> arbitrary <*> arbitrary)
+            ]
+
+instance Eq a => EqProp (Tree a) where
+    (=-=) = eq
+
+type TI = Tree
 
 main = do
-    let trigger = undefined :: TI ([Int], [Int], Int, Sum Int)
-    quickBatch $ (traversable trigger)
+    let trigger = undefined :: TI (Maybe Int, Maybe Int, Int, Sum Int)
+    quickBatch $ traversable trigger
