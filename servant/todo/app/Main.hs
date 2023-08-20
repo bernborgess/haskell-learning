00bd8be67e12c74ef8f9182d1344b64ad98a6ec3
bernborgess <bernborgess@outlook.com>
2023-08-20 18:22:48 -0300

    dotenv works

--- servant/todo/app/Main.hs
+++ servant/todo/app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Configuration.Dotenv (defaultConfig, loadFile)
 import Control.Concurrent (forkIO, killThread)
 import Control.Exception (bracket)
 import Network.HTTP.Client (defaultManagerSettings, newManager)
@@ -8,22 +9,25 @@ import Servant (NoContent)
 import Servant.Client
 import Server (api, startApp)
 import Storage (Message, initDB)
+import System.Environment (getEnv)
 
-postMsg :: Message -> ClientM NoContent
-getMsgs :: ClientM [Message]
-postMsg :<|> getMsgs = client api
+-- postMsg :: Message -> ClientM NoContent
+-- getMsgs :: ClientM [Message]
+-- postMsg :<|> getMsgs = client api
 
 main :: IO ()
 main = do
-    let port = 8080 :: Int
+    _ <- loadFile defaultConfig
+    port <- getEnv "PORT"
+    -- let port = "8080"
+    putStrLn $ "Running in port " ++ show port
     let dbFile = "test.db"
     initDB dbFile
-    mgr <- newManager defaultManagerSettings
-    bracket (forkIO $ startApp port dbFile) killThread $ \_ -> do
-        ms <- flip runClientM (mkClientEnv mgr (BaseUrl Http "localhost" port "")) $ do
-            _ <- postMsg "hello"
-            _ <- postMsg "world"
-            getMsgs
-        print ms
+    -- mgr <- newManager defaultManagerSettings
+    startApp (read port) dbFile
 
--- putStrLn $ "Running in port " ++ show port
+-- ms <- flip runClientM (mkClientEnv mgr (BaseUrl Http "localhost" port "")) $ do
+--     _ <- postMsg "hello"
+--     _ <- postMsg "world"
+--     getMsgs
+-- print ms
