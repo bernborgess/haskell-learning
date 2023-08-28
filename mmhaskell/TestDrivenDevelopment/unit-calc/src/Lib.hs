1f9320cd9698efdd10a16b5bb242b08a25fdcbd9
bernborgess <bernborgess@outlook.com>
2023-08-28 18:16:18 -0300

    initial testing

---
+++ mmhaskell/TestDrivenDevelopment/unit-calc/src/Lib.hs
@@ -0,0 +1,10 @@
+module Lib (
+    someFunc,
+    simpleMathFunction,
+) where
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+simpleMathFunction :: Int -> Int -> Int -> Int
+simpleMathFunction = undefined
---
+++ mmhaskell/TestDrivenDevelopment/unit-calc/test/Spec.hs
@@ -0,0 +1,5 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
+main :: IO ()
+main = putStrLn "Test suite not yet implemented"
