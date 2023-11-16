55034761848a8136e9a0b3f3ae31a2a3787b3ee0
bernborgess <bernborgess@outlook.com>
2023-11-16 18:00:07 -0300

    Monad Transformers Chapter Overview

---
+++ hpffp/26MonadTransformers/1MonadTransformers.hs
@@ -0,0 +1,21 @@
+-- Monad transformers
+-- The last chapter demonstrated why we need monad transformers and
+-- the basic type manipulation that’s going on to make that
+-- bit o’ magick happen.
+
+-- Monad transformers are important in a lot of everyday
+-- Haskell code, though, so we want to dive deeper and make
+-- sure we have a good understanding of how to use them in practice
+
+-- Even after you know how to write all the transformer instances,
+-- managing stacks of transformers in an application can be tricky.
+
+-- The goal of this chapter is to get comfortable with it.
+
+-- In this chapter, we will
+
+-- * work through more monad transformer types and instances;
+
+-- * look at the ordering and wrapping of monad transformer stacks;
+
+-- * lift, lift, lift, and lift some more.
