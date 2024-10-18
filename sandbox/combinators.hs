a0c276b5de58ab8e6bdfbd7ec5daaf4cfa104f1b
bernborgess <bernborgess@outlook.com>
2024-10-18 11:17:55 -0300

    Combinators Conor Hoekstra talk

---
+++ sandbox/combinators.hs
@@ -0,0 +1,26 @@
+import Data.Either (isLeft)
+import Data.Maybe (isNothing)
+import GHC.Base (join)
+
+--
+-- (range 1 8) (map (fn [n] (repeat n n))) (reduce concat)
+assert :: Int -> Bool -> Either Int ()
+assert i True = Right ()
+assert i False = Left i
+
+test :: Either Int ()
+test = do
+  assert 1 $ buildSeq 1 == [1]
+  assert 2 $ buildSeq 2 == [1, 2, 2]
+  assert 3 $ buildSeq 3 == [1, 2, 2, 3, 3, 3]
+
+range :: Int -> Int -> [Int]
+range = enumFromTo
+
+buildSeq :: Int -> [Int]
+buildSeq = concatMap (join replicate) . range 1
+
+main = do
+  case test of
+    Left l -> putStrLn $ "Fails at " ++ show l
+    Right () -> putStrLn "Passes All"
